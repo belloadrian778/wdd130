09fa5563dcfe8e7583c3f6ea24b3c28dc27e960b
belloadrian778 <belloamzna@gmail.com>
2026-01-29 17:19:16 -0500

    aaaaa

---
+++ .vscode/Untitled-1.py
@@ -0,0 +1,29 @@
+#first_number = input ("What is the first number?")
+#second_number = input ("What is the second number?")
+
+#if first_number > second_number:
+#    print ("The first number is greater")
+#else: print("The first number is not greater")
+
+
+#if first_number == second_number:
+#    print ("The numbers are equal")
+#else: print("The numbers are not equal")
+
+#if second_number > first_number:
+#    print ("The second number is greater")
+#else: print("The second number is not greater")
+
+x = 6
+y = 6
+
+if x == 5:
+    print("a")
+
+    if y == 6:
+        print("b")
+else:
+    print("c")
+
+    if y == 10:
+        print("d")
